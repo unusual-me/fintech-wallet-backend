70428a736865cc2a7054dbebef7e2f898abd38b6
HARSH RAJESH NAIKDHURE <harshrajeshnaikdhure@HARSHs-MacBook-Air.local>
2025-11-22 23:06:16 +0530

    feat(wallet): add deposit API with transactional wallet update and transaction logging

--- src/controllers/transaction.controller.ts
+++ src/controllers/transaction.controller.ts
@@ -4,6 +4,7 @@ import { Wallet } from "../models/wallet.model";
 import { Transaction } from "../models/transaction.model";
 import { successResponse, errorResponse } from "../utils/response";
 import { v4 as uuidv4 } from "uuid";
+import { depositSchema} from "../utils/validations/transactionSchema"
 
 export const getTransactionHistory = async (req: Request, res: Response) => {
   try {
@@ -27,10 +28,23 @@ export const deposit = async (req: Request, res: Response) => {
   session.startTransaction();
 
   try {
-    const { walletId, amount, description } = req.body;
-    const wallet = await Wallet.findById(walletId).session(session);
+    /*
+      POST API to deposit amount in user's wallet
+    */
+
+    // Payload Validation
+    const { error, value } = depositSchema.validate(req.body, { abortEarly: false });
+    if (error) {
+      return errorResponse(res, error.details.map(e => e.message).join(", "), 400);
+    }
+
+    const { userId, amount, description } = value;
+
+    // Fetch wallet
+    const wallet = await Wallet.findOne({ user: userId }).session(session);
     if (!wallet) return errorResponse(res, "Wallet not found", 404);
 
+    // Update balance
     wallet.balance += amount;
 
     const tx = await Transaction.create(
@@ -48,13 +62,26 @@ export const deposit = async (req: Request, res: Response) => {
     );
 
     await wallet.save({ session });
+
+    const ele = tx[0];
+    const response = {
+      amount: ele.amount,
+      type: ele.type,
+      referenceId: ele.referenceId,
+      description: ele.description,
+      transactionId: ele._id,
+      createdAt: ele.createdAt,
+      updatedAt: ele.updatedAt,
+      newBalance: wallet.balance,
+    };
+
     await session.commitTransaction();
 
-    return successResponse(res, tx[0], "Deposit successful");
-  } catch (err) {
+    return successResponse(res, response, "Deposit successful");
+  } catch (err: any) {
     await session.abortTransaction();
-    console.error(err);
-    return errorResponse(res, "Failed to process deposit");
+    console.error("deposit error:", err);
+    return errorResponse(res, err.message || "Failed to process deposit", 500);
   } finally {
     session.endSession();
   }
